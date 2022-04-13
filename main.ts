9aefeb6fb321af3442764c66dd0f36fec3f387e1
qwq-789 <77134549+qwq-789@users.noreply.github.com>
2022-04-13 19:09:46 +0800

    done

--- main.ts
+++ main.ts
@@ -5,30 +5,55 @@ namespace SpriteKind {
     export const coin = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.arrow, SpriteKind.Enemy, function (sprite, otherSprite) {
-    coin = sprites.create(img`
-        . . b b b b . . 
-        . b 5 5 5 5 b . 
-        b 5 d 1 1 d 5 b 
-        b 5 1 5 5 1 5 b 
-        c 5 1 5 5 1 5 c 
-        c 5 d 1 1 5 5 c 
-        . f 5 5 5 5 f . 
-        . . f f f f . . 
-        `, SpriteKind.coin)
-    coin.setPosition(otherSprite.x, otherSprite.y)
-    coin.z = -1
     sprite.destroy()
     virusHeart += ATT
     if (virusHeart == 0) {
+        _100dollars = sprites.create(img`
+            f f f f f f f f f f f f f f f f f f f f 
+            f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 f 
+            f 3 2 3 2 2 2 3 2 2 2 3 3 3 3 3 3 3 3 f 
+            f 3 2 3 2 3 2 3 2 3 2 3 3 3 3 3 3 3 3 f 
+            f 3 2 3 2 2 2 3 2 2 2 3 3 3 2 2 2 3 3 f 
+            f 3 3 3 3 3 3 3 3 3 3 3 3 3 2 2 2 3 3 f 
+            f 3 2 3 3 3 3 3 3 3 3 3 3 3 2 2 2 3 3 f 
+            f 3 3 3 3 3 3 3 3 3 3 3 3 3 3 2 3 3 3 f 
+            f 3 3 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 3 f 
+            f f f f f f f f f f f f f f f f f f f f 
+            `, SpriteKind.coin)
+        scaling.scaleByPercent(_100dollars, 16, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+        _100dollars.setPosition(otherSprite.x, otherSprite.y)
+        _100dollars.z = -1
         otherSprite.startEffect(effects.disintegrate, 200)
         otherSprite.destroy()
+    }
+    if (otherSprite == virusBoss) {
+        virusBossHeart += ATT
+        if (virusBossHeart == 0) {
+            _1000dollars = sprites.create(img`
+                f f f f f f f f f f f f f f f f f f f f 
+                f 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d f 
+                f 9 8 9 8 8 8 9 8 8 8 9 8 8 8 9 9 9 d f 
+                f 9 8 9 8 9 8 9 8 9 8 9 8 9 8 9 9 9 d f 
+                f 9 8 9 8 8 8 9 8 8 8 9 8 8 8 9 9 9 d f 
+                f 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 d f 
+                f 9 8 9 9 9 9 8 8 9 8 8 9 9 9 9 9 9 d f 
+                f 9 9 9 9 9 9 8 8 9 8 8 9 8 8 9 9 9 d f 
+                f 9 9 9 9 9 9 9 8 9 9 8 9 8 8 9 9 9 d f 
+                f f f f f f f f f f f f f f f f f f f f 
+                `, SpriteKind.coin)
+            _1000dollars.setPosition(otherSprite.x, otherSprite.y)
+            scaling.scaleByPercent(_1000dollars, 24, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+            _100dollars.z = -1
+            otherSprite.startEffect(effects.disintegrate, 200)
+            otherSprite.destroy()
+        }
     }
     music.zapped.play()
 })
 function doMap () {
     tiles.setCurrentTilemap(tilemap`層級1`)
     for (let value of tiles.getTilesByType(assets.tile`我的貼圖`)) {
-        tree = sprites.create(img`
+        tree2 = sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . c c c c 6 . . . . . 
             . . . . c c 6 7 7 5 5 6 6 . . . 
@@ -46,7 +71,7 @@ function doMap () {
             . . . . . . e e e . . 4 . . . . 
             . . . . . . . e e . e . . . . . 
             `, SpriteKind.tree)
-        tiles.placeOnTile(tree, value)
+        tiles.placeOnTile(tree2, value)
         tiles.setTileAt(value, sprites.skillmap.islandTile4)
         tiles.setWallAt(value, true)
     }
@@ -57,7 +82,6 @@ function plot () {
     game.splash("並延長你難得的外出時間")
     runText = true
     runText2 = true
-    counter = 1
 }
 function end () {
     game.splash("你感染了過量病毒，", "需要在家隔離")
@@ -65,7 +89,6 @@ function end () {
     game.over(false)
 }
 function weaponLevelup () {
-    ATT = -1
     if (info.score() <= 9) {
         runText2 = true
     } else if (info.score() >= 15 && info.score() <= 19) {
@@ -221,10 +244,12 @@ function virusLevelup () {
             game.splash("小心!", "病毒的抗藥性上升了")
             runText = false
         }
-    } else if (timeSec == 600) {
+    } else if (timeSec == 5) {
         if (runText) {
-            virusHeart = 1000
+            virusHeart = 80
             game.splash("小心!", "病毒的抗藥性大幅度上升了")
+            game.splash("並且出現了新型變種")
+            VirusBoss()
             runText = false
         }
     }
@@ -232,13 +257,16 @@ function virusLevelup () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
     otherSprite.startEffect(effects.confetti, 200)
     otherSprite.destroy()
-    info.changeScoreBy(1)
+    if (otherSprite == _1000dollars) {
+        info.changeScoreBy(10)
+    } else {
+        info.changeScoreBy(1)
+    }
     runText = true
     music.magicWand.play()
 })
 function Virus () {
     virus = sprites.create(assets.image`virus`, SpriteKind.Enemy)
-    virusHeart = 1
     animation.runImageAnimation(
     virus,
     assets.animation`我的動畫`,
@@ -282,19 +310,42 @@ function walkAnimate () {
     )
 }
 function doCharacter () {
+    virusHeart = 1
     charater = sprites.create(assets.image`standD`, SpriteKind.Player)
     info.setLife(3)
     info.setScore(0)
     controller.moveSprite(charater)
     charater.setPosition(510, 510)
-    charater.setBounceOnWall(true)
     scene.cameraFollowSprite(charater)
+    ATT = -1
     cd = 2000
+}
+function VirusBoss () {
+    virusBoss = sprites.create(assets.image`virus2`, SpriteKind.Enemy)
+    scaling.scaleByPercent(virusBoss, 50, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+    virusBossHeart = 200
+    animation.runImageAnimation(
+    virusBoss,
+    assets.animation`我的動畫0`,
+    150,
+    true
+    )
+    virusBoss.setPosition(randint(0, 1020), randint(0, 1020))
+    if (spriteutils.distanceBetween(virusBoss, charater) < 30) {
+        virusBoss.destroy()
+        VirusBoss()
+    } else {
+        virusBoss.follow(charater, 40)
+    }
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.startEffect(effects.disintegrate, 200)
     otherSprite.destroy()
-    info.changeLifeBy(-1)
+    if (otherSprite == virusBoss) {
+        info.changeLifeBy(-3)
+    } else {
+        info.changeLifeBy(-1)
+    }
     music.powerDown.play()
 })
 let vaccine: Sprite = null
@@ -303,13 +354,15 @@ let charater: Sprite = null
 let cd = 0
 let timeSecWord = ""
 let timeMinterWord = ""
-let counter = 0
 let runText2 = false
 let runText = false
-let tree: Sprite = null
+let tree2: Sprite = null
+let _1000dollars: Sprite = null
+let virusBossHeart = 0
+let virusBoss: Sprite = null
+let _100dollars: Sprite = null
 let ATT = 0
 let virusHeart = 0
-let coin: Sprite = null
 let timeSec = 0
 let timeMinter = 0
 timeMinter = 0
@@ -349,8 +402,8 @@ forever(function () {
         b b b b b b b b 
         `, charater, 100, 0)
     spriteutils.setVelocityAtAngle(vaccine, randint(0, 360), 100)
-    for (let value of spriteutils.getSpritesWithin(SpriteKind.Enemy, 100, charater)) {
-        spriteutils.setVelocityAtAngle(vaccine, spriteutils.angleFrom(charater, value), 100)
+    for (let value2 of spriteutils.getSpritesWithin(SpriteKind.Enemy, 100, charater)) {
+        spriteutils.setVelocityAtAngle(vaccine, spriteutils.angleFrom(charater, value2), 100)
     }
     vaccine.setKind(SpriteKind.arrow)
     weaponLevelup()
